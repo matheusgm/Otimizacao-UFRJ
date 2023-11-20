d08231b229dea06d6b6b1b91ee646fd38df69249
Matheus Guimarães de Moura <math.moura@hotmail.com>
2023-11-20 19:16:38 -0300

    gradient method

--- main.py
+++ main.py
@@ -79,20 +79,59 @@ def armijo(N, gamma, x1, x2, x3, x4, x5, d):
 		t *= gamma
 	return t
 
+def passed_tolerance(d, tol):
+	for i in range(len(d)):
+		if(abs(d[i]) >= tol): return False
+	return True
+
+# Gradient Method
+def gradient_method(x1, x2, x3, x4, x5, tol, max_interation):
+	k = 0
+	N = 0.25
+	gamma = 0.8
+	t = 0
+
+	d = gradiente(x1, x2, x3, x4, x5)
+	for i in range(len(d)):
+		d[i] = -d[i]
+	
+	while ((not passed_tolerance(d, tol)) and k < max_interation):
+		t = armijo(N, gamma, x1, x2, x3, x4, x5, d)
+		x1 = x1 + t * d[0]
+		x2 = x2 + t * d[1]
+		x3 = x3 + t * d[2]
+		x4 = x4 + t * d[3]
+		x5 = x5 + t * d[4]
+		d = gradiente(x1, x2, x3, x4, x5)
+		for i in range(len(d)):
+			d[i] = -d[i]
+		k+=1
+	print(f"Iter: {k} | x1: {x1} | x2: {x2} | x3: {x3} | x4: {x4} | x5: {x5} | value: {funcao(x1, x2, x3, x4, x5)}")
+	return [x1, x2, x3, x4, x5, k]
+
+# Newton Method
+
+# Quase-Newton Method
+
+
+
+
 # print(hessiana(1, 1, 1, 1, 10)[2][3])
+gradient_method(1, 1, 1, 1, 1, 0.00001, 1000)
+
 
 
 # Teste
-def solve_gradient():
-	sym.init_printing()
-	x1,x2,x3,x4,x5 = sym.symbols('x1,x2,x3,x4,x5')
+# def solve_gradient():
+# 	sym.init_printing()
+# 	x1,x2,x3,x4,x5 = sym.symbols('x1,x2,x3,x4,x5')
 
-	dx1 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*(x1 + (sym.exp(x1)-x2)*sym.exp(x1)),0)
-	dx2 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*(-1*(sym.exp(x1)-x2)),0)
-	dx3 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*((x3+x4)+(sym.exp(x3+x4) - x5)*sym.exp(x3+x4)),0)
-	dx4 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*((x3+x4)+(sym.exp(x3+x4) - x5)*sym.exp(x3+x4)),0)
-	dx5 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*(-1*(sym.exp(x3+x4) - x5)),0)
+# 	dx1 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*(x1 + (sym.exp(x1)-x2)*sym.exp(x1)),0)
+# 	dx2 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*(-1*(sym.exp(x1)-x2)),0)
+# 	dx3 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*((x3+x4)+(sym.exp(x3+x4) - x5)*sym.exp(x3+x4)),0)
+# 	dx4 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*((x3+x4)+(sym.exp(x3+x4) - x5)*sym.exp(x3+x4)),0)
+# 	dx5 = sym.Eq(((x1**2 + (sym.exp(x1) - x2)**2 + (x3 + x4)**2 + (sym.exp(x3+x4) - x5)**2)**(-0.5))*(-1*(sym.exp(x3+x4) - x5)),0)
 
-	print(sym.solve([dx1,dx2,dx3,dx4,dx5],(x1,x2,x3,x4,x5)))
+# 	print(sym.solve([dx1,dx2,dx3,dx4,dx5],(x1,x2,x3,x4,x5)))
 
-solve_gradient()
+# solve_gradient()
